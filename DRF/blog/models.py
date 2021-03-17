bf32445df39fc4b10a5772bc58830868b86ed2be
Keith <kg.anderson@outlook.com>
2021-03-17 17:10:47 +1300

    create users table and add simple JWT

--- DRF/blog/models.py
+++ DRF/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from django.conf import settings
 
 
 class Category(models.Model):
@@ -20,16 +20,20 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1)
     title = models.CharField(max_length=250)
     excerpt = models.TextField(null=True)
     content = models.TextField()
     slug = models.SlugField(max_length=250, unique_for_date='published')
     published = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    status = models.CharField(max_length=10, choices=options, default='published')
-    objects = models.Manager()  
-    postobjects = PostObjects() 
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(
+        max_length=10, choices=options, default='published')
+    objects = models.Manager()  # default manager
+    postobjects = PostObjects()  # custom manager
 
     class Meta:
         ordering = ('-published',)
